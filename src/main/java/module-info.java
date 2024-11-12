8dabb0266373323e4006389c67c4ebf192a7c3f9
Mike Hearn <mike@hydraulic.software>
2024-11-12 19:03:21 +0100

    Publish version 1.0

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module io.github.mikehearn.githubdraftreleasetest {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires dev.hydraulic.conveyor.control;
-
-    opens io.github.mikehearn.githubdraftreleasetest to javafx.fxml;
-    exports io.github.mikehearn.githubdraftreleasetest;
-}
